82e5788f6e7a412fb1c9f8994f9e70b7ddd4e248
David Goodwin <david@palepurple.co.uk>
2018-01-29 14:36:38 +0000

    add Predis adapter/support

---
+++ src/Adapter/Predis.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Touhonoob\RateLimit\Adapter;
+
+
+/**
+ * Predis adapter
+ */
+class Predis extends Redis
+{
+
+    /**
+     * @var \Predis\ClientInterface
+     */
+    protected $redis;
+
+    public function __construct(\Predis\ClientInterface $client)
+    {
+        $this->redis = $client;
+    }
+
+    public function set($key, $value, $ttl)
+    {
+        return $this->redis->set($key, $value, "ex", $ttl);
+    }
+}
