bb438c0225b7424df633956b9ed534a318538ae0
David Goodwin <david@palepurple.co.uk>
2018-01-29 14:42:24 +0000

    add predis test, add checks for redis/apc etc extensions and skip tests if not available

--- tests/RateLimitTest.php
+++ tests/RateLimitTest.php
@@ -2,8 +2,8 @@
 
 namespace Touhonoob\RateLimit\Tests;
 
-use Touhonoob\RateLimit\RateLimit;
 use Touhonoob\RateLimit\Adapter;
+use Touhonoob\RateLimit\RateLimit;
 
 /**
  * @author Peter Chung <touhonoob@gmail.com>
@@ -21,6 +21,12 @@ class RateLimitTest extends \PHPUnit_Framework_TestCase
      */
     public function testCheckAPC()
     {
+        if (!extension_loaded('apc')) {
+            $this->markTestSkipped("apc not installed");
+        }
+        if (ini_get('apc.enable_cli') == 0) {
+            $this->markTestSkipped("apc.enable_cli != 1; can't change at runtime");
+        }
         $adapter = new \Touhonoob\RateLimit\Adapter\APC();
         $this->check($adapter);
     }
@@ -30,24 +36,48 @@ class RateLimitTest extends \PHPUnit_Framework_TestCase
      */
     public function testCheckAPCu()
     {
+        if (!extension_loaded('apcu')) {
+            $this->markTestSkipped("apcu not installed");
+        }
+        if (ini_get('apc.enable_cli') == 0) {
+            $this->markTestSkipped("apc.enable_cli != 1; can't change at runtime");
+        }
         $adapter = new \Touhonoob\RateLimit\Adapter\APCu();
         $this->check($adapter);
     }
 
     /**
      * @requires extension redis
-     */ 
+     */
     public function testCheckRedis()
     {
+        if (!extension_loaded('redis')) {
+            $this->markTestSkipped("Redis not installed");
+        }
         $adapter = new \Touhonoob\RateLimit\Adapter\Redis();
         $this->check($adapter);
     }
 
     public function testCheckRedisCustomClient()
     {
+        if (!extension_loaded('redis')) {
+            $this->markTestSkipped("redis extension not installed");
+        }
         $redis = new \Redis();
         $redis->pconnect('127.0.0.1', 6379);
         $adapter = new \Touhonoob\RateLimit\Adapter\RedisCustomClient($redis);
+        $this->check($adapter);
+    }
+
+    public function testCheckPredisClient()
+    {
+        if (!class_exists(\Predis\Client::class)) {
+
+            $this->markTestSkipped("no predis/predis?");
+        }
+
+        $predis = new \Predis\Client(); // assumes localhost:6379
+        $adapter = new \Touhonoob\RateLimit\Adapter\Predis($predis);
         $this->check($adapter);
     }
 
@@ -57,27 +87,26 @@ class RateLimitTest extends \PHPUnit_Framework_TestCase
         $rateLimit = $this->getRateLimit($adapter);
         $rateLimit->ttl = 100;
 
-        $this->assertEquals(self::MAX_REQUESTS, $rateLimit->getAllow($ip));
+        $rateLimit->purge($ip); // incase the previous test failed and our storage is dirty.
 
-        //First
-        $this->assertEquals(self::MAX_REQUESTS, $rateLimit->check($ip));
 
-        //Repeat MAX_REQUESTS - 1 times
+        //Repeat MAX_REQUESTS - 1 times (all should work, but bucket should be empty at the end)
         for ($i = 0; $i < self::MAX_REQUESTS; $i++) {
-            $this->assertEquals(self::MAX_REQUESTS - $i - 1, $rateLimit->getAllow($ip));
-            $this->assertEquals(self::MAX_REQUESTS - $i - 1, $rateLimit->check($ip));
+            // calling check reduces the counter each time.
+            $this->assertEquals((self::MAX_REQUESTS - $i), $rateLimit->getAllow($ip));
+            $this->assertTrue($rateLimit->check($ip));
         }
 
-        //MAX_REQUESTS + 1
         $this->assertEquals(0, $rateLimit->getAllow($ip));
-        $this->assertEquals(0, $rateLimit->check($ip));
+        $this->assertFalse($rateLimit->check($ip));
 
-        //Wait for PERIOD seconds
+
+        //Wait for PERIOD seconds, bucket should refill
         sleep(self::PERIOD);
+
         $this->assertEquals(self::MAX_REQUESTS, $rateLimit->getAllow($ip));
         $this->assertEquals(self::MAX_REQUESTS, $rateLimit->check($ip));
 
-        $rateLimit->purge($ip);
     }
 
     private function getRateLimit(Adapter $adapter)
