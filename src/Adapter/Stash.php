1e787daa767a1358284cf2cff73861378e3a1526
David Goodwin <david@palepurple.co.uk>
2018-02-12 10:14:07 +0000

    add Stash adapter

---
+++ src/Adapter/Stash.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Touhonoob\RateLimit\Adapter;
+
+
+use Touhonoob\RateLimit\Adapter;
+
+class Stash extends Adapter
+{
+
+    /**
+     * @var \Stash\Pool
+     */
+    private $pool;
+
+    public function __construct(\Stash\Pool $pool)
+    {
+        $this->pool = $pool;
+    }
+
+    public function get($key)
+    {
+        $item = $this->pool->getItem($key);
+        if ($item->isHit()) {
+            return $item->get();
+        }
+        return null;
+    }
+
+    public function set($key, $value, $ttl)
+    {
+        $item = $this->pool->getItem($key);
+        $item->setTtl($ttl);
+        $item->set($value);
+        $item->save();
+    }
+
+    public function exists($key)
+    {
+        return $this->pool->hasItem($key);
+    }
+
+    public function del($key)
+    {
+        $this->pool->deleteItem($key);
+    }
+}
